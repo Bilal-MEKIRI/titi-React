7c9aa3024496a3685531e3fc42b76c1ba51f09de
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-03-06 21:04:21 +0100

    click and collect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Slide from "./components/slide";
 import Template from "./components/template";
+import Collect from "./pages/click&collect";
 import Home from "./pages/homePage";
 import Burger from "./pages/pageBurgers";
 
@@ -11,7 +12,8 @@ function App() {
       <Route path="/" element={<Template />}>
         {/* <Route index element={<Burger />}></Route> */}
         {/* <Route index element={<Slide />}></Route> */}
-        <Route index element={<Home />}></Route>
+        {/* <Route index element={<Home />}></Route> */}
+        <Route index element={<Collect />}></Route>
       </Route>
     </Routes>
   );
---
+++ src/pages/click&collect/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Collect() {
+  return (
+    <main>
+      <section class="clickAndCollect">
+        <h1>Click & Collect</h1>
+        <img id="imgLigne" src="images\Line 11.png" alt="ligne verticale" />
+        <h2>Commander et déguster</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias
+          eligendi labore, nihil, sapiente assumenda dolores ipsum, hic animi
+          expedita fugit quia maxime a impedit delectus debitis est perferendis.
+          Temporibus, neque?
+        </p>
+      </section>
+      <div id="lastSection">
+        <section id="section">
+          <a href="commande1.html">
+            <button id="commencerButton">Commencer</button>
+          </a>
+        </section>
+      </div>
+    </main>
+  );
+}
+export default Collect;
